4febcc4fcac76648e0fba7595761d7b9f0aa1dab
val8888 <volkermarcos1@gmail.com>
2023-01-19 16:39:30 -0300

    vaaaamo

---
+++ api/src/models/Diet.js
@@ -0,0 +1,16 @@
+const sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+      sequelize.define('diet', {
+            name: {
+                  type: DataTypes.STRING,
+                  allowNull: false,
+            },
+            id: {
+                  type: DataTypes.UUID,
+                  defaultValue: DataTypes.UUIDV4,
+                  primaryKey: true,
+            },
+      });
+};
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -2,11 +2,32 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+      // defino el modelo
+      sequelize.define('recipe', {
+            title: {
+                  type: DataTypes.STRING,
+                  allowNull: false,
+                  unique: true,
+            },
+            id: {
+                  type: DataTypes.INTEGER,
+                  primaryKey: true,
+                  autoIncrement: true,
+            },
+            image: {
+                  type: DataTypes.STRING,
+                  defaultValue:
+                        'https://img.favpng.com/22/2/12/catering-food-computer-icons-logo-event-management-png-favpng-4ju2xAb25XdBdKZzyZprTjPkn.jpg',
+            },
+            summary: {
+                  type: DataTypes.TEXT,
+                  allowNull: false,
+            },
+            healthScore: {
+                  type: DataTypes.INTEGER,
+            },
+            analyzedInstructions: {
+                  type: DataTypes.TEXT,
+            },
+      });
 };
